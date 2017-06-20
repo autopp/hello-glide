e2922473e0bb8efc270b942df7d70ce0e6ab9c2e
autopp <autopp.inc@gmail.com>
2017-06-20 09:53:11 +0900

    add version and hello command

--- main.go
+++ main.go
@@ -15,7 +15,28 @@ var RootCmd = &cobra.Command{
     },
 }
 
+var versionCmd = &cobra.Command{
+    Use:   "version",
+    Short: "Print the version number",
+    Long:  `All software has versions.`,
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("hello-glide v1.0")
+    },
+}
+
+var helloCmd = &cobra.Command{
+    Use:   "hello",
+    Short: "hello someone",
+    Long:  "say hello to.",
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("hello " + args[0])
+    },
+}
+
 func main() {
+    cobra.OnInitialize()
+    RootCmd.AddCommand(versionCmd)
+    RootCmd.AddCommand(helloCmd)
     if err := RootCmd.Execute(); err != nil {
         fmt.Println(err)
         os.Exit(-1)
